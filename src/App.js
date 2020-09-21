73982bd56b63ad18904e5ef987ca89c3f5f114a7
ElluLaitinen <ellu.laitinen85@gmail.com>
2020-09-21 17:38:40 +0300

    finish basic structure

--- src/App.js
+++ src/App.js
@@ -7,21 +7,37 @@ import Hatch from './Hatch';
 
 const GlobalStyle = createGlobalStyle`
 body {
-  background: url('https://source.unsplash.com/7VOyZ0-iO0o')  center / cover  ;
+  background:  center / cover url('https://source.unsplash.com/7VOyZ0-iO0o')  ;
   margin: 0;
 }`
 
-
-
 function App() {
-  const [hatches, setHatches] = useState(createCalendar())
+  const [hatches, setHatches] = useState(createCalendar());
+
+  /*  useEffect(() => {
+     const calendar = localStorage.calendar
+       ? JSON.parse(localStorage.calendar)
+       : createCalendar();
+ 
+     setHatches(calendar);
+   }, []); */
+
+  // store calendar in local storage
+
+  /*   useEffect(() => {
+      hatches.length && localStorage.setItem('calendar', JSON.stringify(hatches));
+    }, []) */
 
   const handleFlipHatch = id => {
+    const updatedHatches = hatches.map(hatch =>
+      hatch.id === id ? { ...hatch, open: !hatch.open } : hatch
+    );
+    setHatches(updatedHatches)
     console.log(id)
   }
   return (
     <>
-      {/*  <GlobalStyle> */}
+      <GlobalStyle />
       <StyledApp>
         {hatches.map(hatch =>
           <Hatch
@@ -31,7 +47,7 @@ function App() {
 
           />)}
       </StyledApp>
-      {/*  </GlobalStyle> */}
+
     </>
   );
 }
