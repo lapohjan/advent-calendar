c5ec309f0a0eebedc4b56975a69ed4ffd2d03abe
ElluLaitinen <ellu.laitinen85@gmail.com>
2020-09-22 10:19:03 +0300

    fix localstorage

--- src/App.js
+++ src/App.js
@@ -12,21 +12,23 @@ body {
 }`
 
 function App() {
-  const [hatches, setHatches] = useState(createCalendar());
+  const [hatches, setHatches] = useState([]);
 
-  /*  useEffect(() => {
-     const calendar = localStorage.calendar
-       ? JSON.parse(localStorage.calendar)
-       : createCalendar();
- 
-     setHatches(calendar);
-   }, []); */
+  useEffect(() => {
+    hatches.length && localStorage.setItem('calendar', JSON.stringify(hatches));
+  }, [hatches])
+
+  useEffect(() => {
+    const calendar = localStorage.calendar
+      ? JSON.parse(localStorage.calendar)
+      : createCalendar();
+
+    setHatches(calendar);
+  }, []);
 
   // store calendar in local storage
 
-  /*   useEffect(() => {
-      hatches.length && localStorage.setItem('calendar', JSON.stringify(hatches));
-    }, []) */
+  /**/
 
   const handleFlipHatch = id => {
     const updatedHatches = hatches.map(hatch =>
