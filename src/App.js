e6d3b686f7b1a845dc7837bf9aa9cad1303eb1fd
lapohjan <59822237+lapohjan@users.noreply.github.com>
2020-09-30 10:04:59 +0300

    modify updateHatches

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
     // console.log(month);
     
     const updatedHatches = hatches.map(hatch =>
-      hatch.id === id &&  hatch.nr <= thisDay && month == 8
+      hatch.id === id &&  hatch.nr <= thisDay && month === 8
       ? { ...hatch, open: !hatch.open } : hatch
       );
 
