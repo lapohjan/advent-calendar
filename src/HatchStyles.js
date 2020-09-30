2de59d8a5ff1ad63b8f6a7daa348c6eaa8267132
lapohjan <59822237+lapohjan@users.noreply.github.com>
2020-09-30 10:15:19 +0300

    modified styles

--- src/HatchStyles.js
+++ src/HatchStyles.js
@@ -5,13 +5,11 @@ export const StyledHatch = styled.div`
 padding-top: 100%;
 position: relative;
 cursor: pointer;
-
 .front {
     background: center / cover url(${props => props.background});
     position: absolute;
     top: 0;
     z-index: 2;
-
     p {
         display: flex;
         align-items: center;
@@ -26,12 +24,10 @@ cursor: pointer;
         font-weight: 700;
         font-size: 4rem;
     }
-
     &.open {
         transform: rotateY(180deg);
     }
 }
-
 .back {
     position: absolute;
     background: center / cover url(${hatchBackdrop});
@@ -39,12 +35,10 @@ cursor: pointer;
     left: 0px;
     z-index: 1;
     transform: rotateY(180deg);
-
     &.open {
         z-index: 2;
         transform: rotateY(0deg);
     }
-
     p {
         font-family: "Dancing Script", cursive;
         color: #fff;
@@ -53,8 +47,7 @@ cursor: pointer;
         text-align: center;
     }
 }
-
-> div {
+.hatch {
     display: flex;
     align-items: center;
     justify-content: center;
@@ -65,7 +58,47 @@ cursor: pointer;
     border-radius: 20px;
     border: 1px solid #fff;
     box-sizing: border-box;
-
 }
-
+.overlay-btn, .closebutton {
+    background: rgba(0, 177, 106, 1);
+    border-radius: 5px;
+    padding: 5px;
+    border: black solid 1px;
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+    font-family: 'Audiowide', cursive;
+    font-weight: lighter;
+    font-size: small;
+}
+#overlay {
+    position: fixed;
+    display: none;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0,0,0,0.5);
+    z-index: 3;
+    cursor: pointer;
+}
+.hatch-content {
+    
+        width: 50%;
+        height: 50%;
+        border: solid black 1px;
+        color: black;
+        background: rgba(200, 247, 197, 0.9);
+        border-radius: 5px;
+        box-shadow: 0 0 5px black; 
+        position: absolute;    
+        padding: 2rem;
+        top: 50%;
+        left: 50%;
+        font-size: 20px;
+        transform: translate(-50%,-50%);
+        -ms-transform: translate(-50%,-50%);   
+}
 `
