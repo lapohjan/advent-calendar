2b22d821ad637dfafa5e3798a3bc874b8e13eccc
lapohjan <59822237+lapohjan@users.noreply.github.com>
2020-10-02 14:04:56 +0300

    add header component

--- src/Header.js
+++ src/Header.js
@@ -1,54 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMusic } from '@fortawesome/free-solid-svg-icons';
+import React, { useState, useEffect } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMusic } from '@fortawesome/free-solid-svg-icons'
 import './Header.css'
+import Music from './Music'
 
-
-// CREATIVE COMMONS LICENSE
-
-// Have A Happy Christmas
-// Exzel Music Publishing (freemusicpublicdomain.com)
-// Licensed under Creative Commons: By Attribution 3.0
-// http://creativecommons.org/licenses/by/3.0/
-
-
-const useAudio = () => {
-// Little idea -song: www.bensound.com
-//   const [audio] = useState(new Audio('/assets/bensound-littleidea.mp3'));
-  const [audio] = useState(new Audio('/assets/geoff-hurly-have-a-happy-christmas.mp3'));
-  const [playing, setPlaying] = useState(false);
-  const toggle = () => setPlaying(!playing);
-
-  useEffect(() => {
-    playing ? audio.play() : audio.pause();
-  });
-
-  useEffect(() => {
-    audio.addEventListener('ended', () => setPlaying(false));
-    return () => {
-      audio.removeEventListener('ended', () => setPlaying(false));
-    };
-  });
-
-  return [playing, toggle];
-};
 
 const Header = () => {
-// Little idea -song: www.bensound.com
-//   const [playing, toggle] = useAudio('/assets/bensound-littleidea.mp3');
-  const [playing, toggle] = useAudio('/assets/geoff-hurly-have-a-happy-christmas.mp3');
   return (
-     <div className="header">
-      {/* <button */}
-            <FontAwesomeIcon
-              onClick={toggle}
-              className="far music"
-              icon={faMusic}
-            >
-              {playing ? 'Pause' : 'Play'}
-            {/* </button> */}
-            </FontAwesomeIcon>
-    </div>
+    <>
+    <Music/>
+    </>
   );
 };
 
