f740290ae467c2729efa921474a674e033babae7
lapohjan <59822237+lapohjan@users.noreply.github.com>
2020-10-02 14:06:23 +0300

    remove unused imports from header

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,4 @@
-import React, { useState, useEffect } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMusic } from '@fortawesome/free-solid-svg-icons'
+import React from 'react'
 import './Header.css'
 import Music from './Music'
 
