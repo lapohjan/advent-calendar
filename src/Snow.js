aed1080a59444361ff259b6899303dc6695d88f6
lapohjan <59822237+lapohjan@users.noreply.github.com>
2020-10-02 13:58:23 +0300

    add snow icon

--- src/Snow.js
+++ src/Snow.js
@@ -1,20 +1,25 @@
-import React, { useEffect } from 'react'
-import { StyledSnow } from './SnowStyles'
-
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSnowflake } from '@fortawesome/free-solid-svg-icons';
+import './Snow.css'
 
 const Snow = () => {
-    const [snowing, setSnowing] = React.useState(false);
+    const [snowing, setSnowing] = useState(false);
 
     return (
     <>  
-        <StyledSnow>
-        <button
+        {/* <button */}
+        <FontAwesomeIcon
                 onClick={() => {
                 setSnowing(!snowing);
                 }}
+                className="far.snowflake"
+                style={{color: "white"}}
+                icon={faSnowflake}
             >
                 Snow!
-            </button>
+            </FontAwesomeIcon>
+            {/* </button> */}
             <div className={snowing ? "snowflakes" : "noSnowflakes"}>
                     <div className="snowflake">
                     ❆
@@ -62,7 +67,6 @@ const Snow = () => {
                     ❆
                     </div>
             </div>
-        </StyledSnow>
     </>
     )
 }
