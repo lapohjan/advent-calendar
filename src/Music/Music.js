09bcf524a1855504ef9a222c93dd5d891de0dd7d
lapohjan <59822237+lapohjan@users.noreply.github.com>
2020-10-08 11:01:42 +0300

    change music and snow css to scss

--- src/Music/Music.js
+++ src/Music/Music.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMusic } from '@fortawesome/free-solid-svg-icons';
-import './Music.css'
+import './Music.scss'
 
 // CREATIVE COMMONS LICENSE
 
