35a84464be5e8f64af25e28a85e5d76a3e9dcd1b
kata-v <7273558+kata-v@users.noreply.github.com>
2020-10-26 14:26:55 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,1 @@
-import firebase from 'firebase';
-import 'firebase/firestore';
-const config = {
-  apiKey: "AIzaSyDJXaZK5KClNmno-FZGf8TI7TRFkggdoQY",
-  authDomain: "my-advent-calendar.firebaseapp.com",
-  databaseURL: "https://my-advent-calendar.firebaseio.com",
-  projectId: "my-advent-calendar",
-  storageBucket: "my-advent-calendar.appspot.com",
-  messagingSenderId: "620045436717",
-  appId: "1:620045436717:web:ed7875a4097839635df5a4",
-  measurementId: "G-CQVEL7J55T"
-};
-// Initialize Firebase
-firebase.initializeApp(config);
-firebase.analytics();
-export default firebase;
+// Add firebase details here
