60d611bed86289dc5539391cfb394c836f5420ed
ElluLaitinen <ellu.laitinen85@gmail.com>
2020-10-26 10:10:45 +0200

    increase footer responsiveness

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className="footer">
       <p className="licence-1">@Laitinen - Pohjanpalo - Vuorinen 2020</p>
-      <p className="licence-2">Music: Have A Happy Christmas, Exzel Music Publishing (freemusicpublicdomain.com). Licensed under Creative Commons: By Attribution 3.0 http://creativecommons.org/licenses/by/3.0/.</p>
+      <p className="licence-2">Music: Have A Happy Christmas, Exzel Music Publishing (freemusicpublicdomain.com). Licensed under Creative Commons: By Attribution 3.0 <a className="img-link" href="http://creativecommons.org/licenses/by/3.0/" target="_blank" rel="noopener noreferrer">http://creativecommons.org/licenses/by/3.0/</a>.</p>
       <p className="licence-3">Favicon from https://icons8.com.</p>
     </div>
   );
